ce4f2a730d75eedb2228682497bcec7d58c803bc
Sukhdev Hansda <sukhdevhansda8@gmail.com>
2022-12-13 14:03:34 +0530

    Install important packages and create user route

---
+++ backend/routes/user.route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const userRouter = express.Router();
+
+userRouter.post('/signup',(req,res)=>{})
+
+userRouter.post("/login", (req, res) => {});
+
+
+module.exports = userRouter;
