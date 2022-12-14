0f813691e8ec99409912b40cc7a00c52d89dfe08
Sukhdev Hansda <sukhdevhansda8@gmail.com>
2022-12-14 19:47:53 +0530

    Fw17 0631 day 2 (#13)
    * Created men route and models
    
    * men shema design and simple GET request endpoints done

--- backend/models/Mentshirt.model.js
+++ backend/models/Mentshirt.model.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
 
 
-const mentshirtSchema = mongoose.Schema({});
+
+const mentshirtSchema = mongoose.Schema({
+  cutoffPrice: { type: String, required: false },
+  price: { type: String, required: false },
+  discount: { type: String, required: true },
+  lazyimgsrc: { type: String, required: true },
+  lazyimgsrc2: { type: String, required: false },
+  imgsrc: { type: String, required: true },
+  nwchide: { type: String, required: false },
+  nwchide2: { type: String, required: false },
+  nwchide3: { type: String, required: true },
+  nwchide4: { type: String, required: true },
+  nwchide5: { type: String, required: false },
+  nwproductviewhref: { type: String, required: false },
+  productpromotions: { type: String, required: false },
+  producttagstext: { type: String, required: false }
+});
 
 
-const Mentshirt = mongoose.model("menstshirt",mentshirtSchema);
+const Mentshirt = mongoose.model("mentshirt",mentshirtSchema);
+
 
 module.exports = Mentshirt;
