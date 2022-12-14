192716d0899ea3f0d982f014ae649de4ea63249f
unknown <mukulsomukesh@gmail.com>
2022-12-14 20:25:24 +0530

    design products page

---
+++ frontend/src/Pages/Products.jsx
@@ -0,0 +1,33 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import {Flex, Stack, HStack, VStack, Box } from '@chakra-ui/react'
+import Filters from '../Components/Products/Filters'
+import ProductsGrid from '../Components/Products/ProductsGrid'
+import Sort from '../Components/Products/Sort'
+
+function Products() {
+    return (
+        // chakra provider
+        <ChakraProvider>
+
+            <Flex w="full" pl="5%" pt="10" gap={"2"}>
+
+                {/* filter component */}
+                <Box w={{ base: '35%', md: '16%', lg: '16%' }} >
+                    <Filters />
+                </Box>
+
+                {/* product grid & sorting components */}
+                <Box w={{ base: '60%', md: '79%', lg: '79%' }} pt="5">
+                    {/* sorting component */}
+                    <Sort />
+
+                    {/* Product grid */}
+                    <ProductsGrid />
+
+                </Box>
+            </Flex>
+
+        </ChakraProvider>)
+}
+
+export default Products
