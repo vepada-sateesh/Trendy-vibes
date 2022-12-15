e565c15a1e23cf3b98602193274bd9c60161850a
ImDebabrata <debabratakgt@hotmail.com>
2022-12-15 17:47:38 +0530

    navbar completed with responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
