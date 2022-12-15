a559606e6ce0cef59ebddad57c7820a21dab4cd9
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2022-12-15 21:38:31 +0530

    Added day-3

--- backend/models/User.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = mongoose.Schema({});
-
-
-const UserModel = mongoose.model("user",userSchema);
-
-
-module.exports = UserModel;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./Components/Authentication/Navbar";
+import Signup from "./Components/Authentication/Signup/Signup";
+import Login from "./Components/Authentication/Login/Login";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <Routes>
+        <Route
+          path="/"
+          element={
+            <ChakraProvider>
+              <Navbar />
+            </ChakraProvider>
+          }
+        ></Route>
+        <Route
+          path="/signup"
+          element={
+            <ChakraProvider>
+              <Signup />
+            </ChakraProvider>
+          }
+        ></Route>
+        <Route
+          path="/login"
+          element={
+            <ChakraProvider>
+              <Login />
+            </ChakraProvider>
+          }
+        ></Route>
+      </Routes> */}
+    </div>
+  );
 }
 
 export default App;
