4a032519e28d7f4bf166468f512294061c4b88a8
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2022-12-16 21:26:17 +0530

    Added day-4

--- backend/index.js
+++ backend/index.js
@@ -5,31 +5,23 @@ const cors = require("cors");
 const connection = require("./config/database");
 const userRouter = require("./routes/user.route");
 
-
 const server = express();
-server.use(cors())
-server.use(express.json())
+server.use(cors());
+server.use(express.json());
 
-server.get("/",(req,res)=>{
-    res.status(200).send({message:"Sephora Homepage API"})
-})
+server.get("/", (req, res) => {
+  res.status(200).send({ message: "Sephora Homepage API" });
+});
 
+server.use("/user", userRouter);
 
-server.use("/user",userRouter);
+server.listen(process.env.PORT, async () => {
+  try {
+    console.log("db is connected successfully");
+  } catch (err) {
+    console.log("db is connected successfully");
+    console.log(err);
+  }
 
-
-
-
-server.listen(process.env.PORT, async()=>{
-
-    try{
-        console.log("db is connected successfully");
-    }
-    catch(err){
-         console.log("db is connected successfully");
-         console.log(err);
-
-    }
-
-    console.log(`Server is listning on http://localhost:${process.env.PORT}`);
-})
+  console.log(`Server is listning on http://localhost:${process.env.PORT}`);
+});
