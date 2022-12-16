9728a8e720cf94e6253446fe95c6cfb383e09847
ImDebabrata <debabratakgt@hotmail.com>
2022-12-17 04:09:40 +0530

    footer done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
