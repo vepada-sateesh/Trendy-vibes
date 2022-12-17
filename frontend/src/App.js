14a3653f7c14fe08b89860ba835ea68169adc9a3
ImDebabrata <debabratakgt@hotmail.com>
2022-12-17 23:50:42 +0530

    day-5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Products from "./Pages/Products";
+import ProductDetails from "./Pages/ProductDetails";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <Products /> */}
+      <ProductDetails />
+    </div>
+  );
 }
 
 export default App;
